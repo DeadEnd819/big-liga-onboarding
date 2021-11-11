363923bb8ec7796d9692284f511cdd92d69e828b
Naymov Aleksey <n.a.v.ru@mail.ru>
2021-11-11 17:06:58 +0300

    Вносит правку

--- source/js/modules/init-intro-to-mobile.js
+++ source/js/modules/init-intro-to-mobile.js
@@ -1,11 +1,13 @@
 const initIntroToMobile = () => {
+  const DESKTOP_WIDTH = '1024';
+
   const intro = document.querySelector('[data-intro]');
   const content = document.querySelector('[data-content]');
 
   const handleScreenClick = () => content.classList.toggle('intro__col--hide');
 
   const addEvent = () => {
-    if (window.innerWidth < '1024') {
+    if (window.innerWidth < DESKTOP_WIDTH) {
       intro.addEventListener('click', handleScreenClick);
       return;
     }
